9a01bf8b9863adb5a8fed13de141f016326a917a
Rishabh Singh Chauhan <143814425+Rsc2414@users.noreply.github.com>
2025-07-20 22:38:03 +0530

    Update 001. Say Hello, World! With Cpp.cpp

--- C++/01. Introduction/001. Say Hello, World! With Cpp.cpp
+++ C++/01. Introduction/001. Say Hello, World! With Cpp.cpp
@@ -1,10 +1,12 @@
 // Problem: https://www.hackerrank.com/challenges/cpp-hello-world/problem
 // Score: 10
 
-
-#include <iostream>
+#include <iostream> // This allows us to use input and output (cin, cout)
 
 int main() {
+    // Print "Hello, World!" to the output
     std::cout << "Hello, World!";
+
+    // Return 0 to indicate the program ended successfully
     return 0;
 }
